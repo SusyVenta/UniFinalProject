315dbd3517a1c6acf53c538413fd86a57f590012
Susanna Ventafridda <susanna.ventafridda@gmail.com>
2023-06-29 19:56:49 +0200

    Completed unit tests for auth utilities

--- test/functions/utils/authUtils.test.js
+++ test/functions/utils/authUtils.test.js
@@ -1,42 +1,148 @@
 import { describe, it, mock, test } from 'node:test';
 import assert from 'node:assert';
+import { attachCsrfToken, getUserSessionDetails } from '../../../functions/utils/authUtils.js'; 
 
-test('synchronous passing test', (t) => {
-    // This test passes because it does not throw an exception.
-    assert.strictEqual(1, 1);
-});
+describe('attachCsrfToken', () => {
+  it('should set the cookie value when the request URL matches', () => {
+    const url = '/example-url';
+    const cookie = 'csrfToken';
+    const value = 'example-value';
 
-test('asynchronous passing test', async (t) => {
-    // This test passes because the Promise returned by the async
-    // function is not rejected.
-    assert.strictEqual(1, 1);
-});
+    const req = {
+      url: '/example-url',
+    };
 
-describe('A thing', () => {
-    it('should work', () => {
-        assert.strictEqual(1, 1);
-    });
+    let cookieValue = '';
+    const res = {
+      cookie: (name, val) => {
+        cookieValue = val;
+      },
+    };
 
-    it('should be ok', () => {
-        assert.strictEqual(2, 2);
-    });
+    let nextCalled = false;
+    const next = () => {
+      nextCalled = true;
+    };
 
-});
+    const middleware = attachCsrfToken(url, cookie, value);
+    middleware(req, res, next);
 
-test('spies on a function', () => {
-    const sum = mock.fn((a, b) => {
-      return a + b;
-    });
-  
-    assert.strictEqual(sum.mock.calls.length, 0);
-    assert.strictEqual(sum(3, 4), 7);
-    assert.strictEqual(sum.mock.calls.length, 1);
-  
-    const call = sum.mock.calls[0];
-    assert.deepStrictEqual(call.arguments, [3, 4]);
-    assert.strictEqual(call.result, 7);
-    assert.strictEqual(call.error, undefined);
-  
-    // Reset the globally tracked mocks.
-    mock.reset();
+    assert.strictEqual(cookieValue, value);
+    assert.strictEqual(nextCalled, true);
   });
+
+  it('should not set the cookie value when the request URL does not match', () => {
+    const url = '/example-url';
+    const cookie = 'csrfToken';
+    const value = 'example-value';
+
+    const req = {
+      url: '/different-url',
+    };
+
+    let cookieValue = '';
+    const res = {
+      cookie: (name, val) => {
+        cookieValue = val;
+      },
+    };
+
+    let nextCalled = false;
+    const next = () => {
+      nextCalled = true;
+    };
+
+    const middleware = attachCsrfToken(url, cookie, value);
+    middleware(req, res, next);
+
+    assert.strictEqual(cookieValue, '');
+    assert.strictEqual(nextCalled, true);
+  });
+});
+
+
+describe('getUserSessionDetails', () => {
+  it('should return authenticated user session details when session cookie is valid', async () => {
+    const adminAuth = {
+      verifySessionCookie: mock.fn(async(sessionCookie, checkRevoked) => {
+        // Mock the behavior of adminAuth.verifySessionCookie when session cookie is valid
+        return {
+          iss: 'example-iss',
+          name: 'John Doe',
+          aud: 'example-aud',
+          auth_time: 1624952023,
+          user_id: 'example-user-id',
+          sub: 'example-sub',
+          iat: 1,
+          exp: 1,
+          email: 'john@example.com',
+          email_verified: true,
+          firebase: { identities: { email: ['example-email'] }, sign_in_provider: 'password' },
+          uid: 'example-uid',
+        };
+      }),
+    };
+
+    const request = {
+      cookies: {
+        __session: 'example-session-cookie',
+      },
+    };
+
+    const result = await getUserSessionDetails(adminAuth, request);
+
+    assert.strictEqual(result.errors, null);
+    assert.deepStrictEqual(result.userSessionDetails, {
+      iss: 'example-iss',
+      name: 'John Doe',
+      aud: 'example-aud',
+      auth_time: 1624952023,
+      user_id: 'example-user-id',
+      sub: 'example-sub',
+      iat: 1,
+      exp: 1,
+      email: 'john@example.com',
+      email_verified: true,
+      firebase: { identities: { email: ['example-email'] }, sign_in_provider: 'password' },
+      uid: 'example-uid',
+    });
+  });
+
+  it('should return null user session details when user is not authenticated', async () => {
+    const adminAuth = {
+      verifySessionCookie: mock.fn(async(sessionCookie, checkRevoked) => {
+        // Mock the behavior of adminAuth.verifySessionCookie when session cookie is invalid or not provided
+        throw new Error('Invalid session cookie');
+      }),
+    };
+
+    const request = {
+      cookies: {},
+    };
+
+    const result = await getUserSessionDetails(adminAuth, request);
+
+    assert.strictEqual(result.errors, null);
+    assert.strictEqual(result.userSessionDetails, null);
+  });
+
+  it('should return null user session details and an error when there is an error', async () => {
+    const adminAuth = {
+      verifySessionCookie: mock.fn(async(sessionCookie, checkRevoked) => {
+        // Mock the behavior of adminAuth.verifySessionCookie when an error occurs
+        throw new Error('Some error occurred');
+      }),
+    };
+
+    const request = {
+      cookies: {
+        __session: 'example-session-cookie',
+      },
+    };
+
+    const result = await getUserSessionDetails(adminAuth, request);
+
+    assert.notStrictEqual(result.errors, null);
+    assert.strictEqual(result.userSessionDetails, null);
+  });
+});
