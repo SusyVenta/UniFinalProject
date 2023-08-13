d7d1ba00c08b87dbfb53a6da5c4f55344e85112e
Susanna Ventafridda <susanna.ventafridda@gmail.com>
2023-08-13 18:19:49 +0200

    Working retrieval of live notifications for currently authenticated user

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -3,27 +3,24 @@
 // Initialize Cloud Firestore and get a reference to the service
 const db = firebase.firestore(firebaseApp);
 
-console.log(auth.currentUser);
 // use emulator DB when testing
 if (location.hostname === "localhost") {
     db.useEmulator("127.0.0.1", 8080);
   }
 
-function getUserNotifications(){
+function getUserNotifications(userID){
     /* 
     Gets data for currently authenticates user in real time. 
     Whenever anything changes in this document, data is pushed to the client.
 
     https://firebase.google.com/docs/firestore/query-data/listen
+
+    This function is called in the footer partial
     */
     
-    //const user = firebase.auth().currentUser;
-    let userID = "Ms56h9q9p1b8QUdBharFxyr0NV53";
     db
     .collection("users")
     .doc(userID).onSnapshot((doc) => {
         console.log("Current data: ", doc.data());
     });
 }
-
-//getUserNotifications()
