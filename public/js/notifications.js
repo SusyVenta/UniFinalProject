5965feb2e63f86f1a07511ffeb14f37b7dc14fa2
Susanna Ventafridda <susanna.ventafridda@gmail.com>
2023-08-13 19:06:26 +0200

    correctly displaying notifications

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -12,15 +12,34 @@ function getUserNotifications(userID){
     /* 
     Gets data for currently authenticates user in real time. 
     Whenever anything changes in this document, data is pushed to the client.
+    Add notifications in from most to last recent to notifications dropdown
 
     https://firebase.google.com/docs/firestore/query-data/listen
 
     This function is called in the footer partial
     */
-    
+    let notificationsDropDown = document.getElementById("notifications-dropdown");
+
     db
     .collection("users")
     .doc(userID).onSnapshot((doc) => {
-        console.log("Current data: ", doc.data());
+        let data = doc.data();
+        console.log("Current data: ", data);
+        // update notifications number
+        let notificationsNumber = document.getElementById("number-notifications-badge");
+        notificationsNumber.innerHTML = data.notifications.length;
+
+        // add notifications as dropdown items
+        let notifications = data.notifications;
+        for (let notification of notifications){
+            let li = document.createElement("li");
+            let a = document.createElement("a");
+            a.setAttribute("class", `dropdown-item`);
+            a.setAttribute("href", notification.URL);
+            a.setAttribute("rel", "nofollow");
+            a.textContent = notification.message;
+            li.appendChild(a);
+            notificationsDropDown.prepend(li);
+        }
     });
 }
